ba9b6af0902c750508fff8a5538259e16cd74be2
Frank R. Leotta III <144361478+FrankLeottaIII@users.noreply.github.com>
2024-04-10 17:54:47 -0400

    PET file started for semester project

---
+++ PagePET.py
@@ -0,0 +1,11 @@
+#This will be a webpage for my caluclate your own Evapotransparation
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import streamlit as st
+import matplotlib.pyplot as plt
+from streamlit_option_menu import option_menu
+import pandas as pd
+

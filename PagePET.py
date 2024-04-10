e77bfb39f2528cb323baf9088d2720e5321e4f75
Frank R. Leotta III <144361478+FrankLeottaIII@users.noreply.github.com>
2024-04-10 18:39:00 -0400

    Temp c added

--- PagePET.py
+++ PagePET.py
@@ -31,53 +31,57 @@ def I_value(Temp_value)->float:
     I_value = (Temp_value/5)**1.5
     return I_value
 
-Temp_January = input("tell me the average Temperature F for January")
+def Convert_to_Celsius(Temp_value)->float:
+    Celsius = (Temp_value-32)*5/9
+    return Celsius
 
-I_January = I_value(Temp_January)
+Temp_January = input("tell me the average Temperature F for January")
+Temp_C_January = Convert_to_Celsius(Temp_January)
+I_January = I_value(Temp_C_January)
 
 Temp_Febuary = input("tell me the average Temperature F for Febuary")
-
-I_Febuary = I_value(Temp_Febuary)
+Temp_C_Febuary = Convert_to_Celsius(Temp_Febuary)
+I_Febuary = I_value(Temp_C_Febuary)
 
 Temp_March = input("tell me the average Temperature F for March")
-
-I_March = I_value(Temp_March)
+Temp_C_March = Convert_to_Celsius(Temp_March)
+I_March = I_value(Temp_C_March)
 
 Temp_April = input("tell me the average Temperature F for April")
-
-I_April = I_value(Temp_April)
+Temp_C_April = Convert_to_Celsius(Temp_April)
+I_April = I_value(Temp_C_April)
 
 Temp_May = input("tell me the average Temperature F for May")
-
-I_May = I_value(Temp_May)
+Temp_C_May = Convert_to_Celsius(Temp_May)
+I_May = I_value(Temp_C_May)
 
 Temp_June = input("tell me the average Temperature F for June")
-
-I_June = I_value(Temp_June)
+Temp_C_June = Convert_to_Celsius(Temp_June)
+I_June = I_value(Temp_C_June)
 
 Temp_July = input("tell me the average Temperature F for July")
-
-I_July = I_value(Temp_July)
+Temp_C_July = Convert_to_Celsius(Temp_July)
+I_July = I_value(Temp_C_July)
 
 Temp_August = input("tell me the average Temperature F for August")
-
-I_August = I_value(Temp_August)
+Temp_C_August = Convert_to_Celsius(Temp_August)
+I_August = I_value(Temp_C_August)
 
 Temp_September = input("tell me the average Temperature F for September")
-
-I_September = I_value(Temp_September)
+Temp_C_September = Convert_to_Celsius(Temp_September)
+I_September = I_value(Temp_C_September)
 
 Temp_October = input("tell me the average Temperature F for October")
-
-I_October = I_value(Temp_October)
+Temp_C_October = Convert_to_Celsius(Temp_October)
+I_October = I_value(Temp_C_October)
 
 Temp_November = input("tell me the average Temperature F for November")
-
-I_November = I_value(Temp_November)
+Temp_C_November = Convert_to_Celsius(Temp_November)
+I_November = I_value(Temp_C_November)
 
 Temp_December = input("tell me the average Temperature F for December")
-
-I_December = I_value(Temp_December)
+Temp_C_December = Convert_to_Celsius(Temp_December)
+I_December = I_value(Temp_C_December)
 
 
 
